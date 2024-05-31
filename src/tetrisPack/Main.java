4ce7531dd4e098457a25e3f79684252f0b717347
unknown <juaneguiabad1639@gmail.com>
2024-06-01 00:02:48 +0200

    Agregados archivos de sonido y new Runnable (hilo) en el main

--- src/tetrisPack/Main.java
+++ src/tetrisPack/Main.java
@@ -5,28 +5,31 @@ import javax.swing.JFrame;
 
 public class Main extends JFrame {
 
-    public Main() {
-        
-        inicializa();
-    }
-    
-    private void inicializa() {
-        
-        add(new Ventana());
-               
-        setResizable(false);
-        pack();
-        
-        setTitle("Tetris Java by Juan Eguia");
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+	public Main() {
 
-    public static void main(String[] args) {
-        
-        EventQueue.invokeLater(() -> {
-            JFrame principal = new Main();
-            principal.setVisible(true);
-        });
-    }
+		inicializa();
+	}
+
+	private void inicializa() {
+
+		add(new Ventana());
+
+		setResizable(false);
+		pack();
+
+		setTitle("Tetris Java by Juan Eguia");
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				JFrame principal = new Main();
+				principal.setVisible(true);
+			}
+		});
+	}
 }
--- src/tetrisPack/Ventana.java
+++ src/tetrisPack/Ventana.java
@@ -2,23 +2,16 @@ package tetrisPack;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.Toolkit;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
-
-import java.util.ArrayList;
 
 public class Ventana extends JPanel implements ActionListener {
 
